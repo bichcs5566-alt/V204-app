286bde3d5d78dd95aa84e5d5281b886808646809
bichcs5566-alt <bichcs5566@gmail.com>
2026-04-09 20:33:28 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,11 @@
-const CACHE_NAME = 'v203-cache-v1';
-const URLS = [
-  './v203_mobile_app.html',
-  './manifest.json',
-  './service-worker.js',
-  './icon-192.png',
-  './icon-512.png'
-];
-
+const CACHE_NAME = 'v205-cache-v1';
+const URLS = ['./v205_fixed_url_app.html','./manifest.json','./service-worker.js'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS)).then(()=>self.skipWaiting()));
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))).then(()=>self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))).then(()=>self.clients.claim()));
 });
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request).then(networkResp => {
-      return networkResp;
-    }).catch(() => caches.match('./v203_mobile_app.html')))
-  );
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request).catch(() => caches.match('./v205_fixed_url_app.html'))));
 });
