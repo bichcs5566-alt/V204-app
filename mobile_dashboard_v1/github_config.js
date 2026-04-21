8ae2e7f391ff5382d9c67063c92ce13e12329125
bichcs5566-alt <bichcs5566@gmail.com>
2026-04-21 23:38:30 +0800

    Update github_config.js

--- mobile_dashboard_v1/github_config.js
+++ mobile_dashboard_v1/github_config.js
@@ -1,7 +1,7 @@
-window.GITHUB_CONFIG = {
+const GITHUB_CONFIG = {
   owner: "bichcs5566-alt",
   repo: "V204-app",
-  token: "",
+  token: "請改成你新的 GitHub PAT",
   branch: "main",
   workflows: {
     positionWriteback: "v2_position_writeback.yml",
