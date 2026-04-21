87d7e73773bab43a6861c08133d357836883401d
bichcs5566-alt <bichcs5566@gmail.com>
2026-04-21 23:33:59 +0800

    Update github_config.js

--- mobile_dashboard_v1/github_config.js
+++ mobile_dashboard_v1/github_config.js
@@ -1,7 +1,7 @@
 window.GITHUB_CONFIG = {
   owner: "bichcs5566-alt",
   repo: "V204-app",
-  token: "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",
+  token: "",
   branch: "main",
   workflows: {
     positionWriteback: "v2_position_writeback.yml",
